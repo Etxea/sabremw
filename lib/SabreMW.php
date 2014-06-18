9f75c440e76b4934d29369b9ec4a766d21f41e61
Jon Latorre <jonlatorremartinez@gmail.com>
2014-06-18 13:42:18 +0200

    codiogo sabre de oc

--- lib/SabreMW.php
+++ lib/SabreMW.php
@@ -40,25 +40,29 @@ class SabreMW
      */
     public function addUser($username,$password)
     {
-        $principal_uri = 'principals/'.$username;
+        //$principal_uri = 'principals/'.$username;
         //Ciframos la password
-        $pass_md5 = md5($username.':SabreDAV:'.$password);
+        $passwordHasher = new Hautelook\Phpass\PasswordHash(8,false);
+        $passwordsalt = '9921b26e612100af3e9f67cdfbc0f5';
+        $pass_md5 = $passwordHasher->HashPassword($password . $passwordsalt);
         $ret = 0;
-        $ret = $this->db->insert('users',array('username'=>$username,'digesta1'=>$pass_md5));
+        $ret = $this->db->insert('oc_users',array('uid'=>$username,'password'=>$pass_md5));
         if ($ret!=1) {
             die("NO se ha podido insertar el user");
         }
+        /*
         //Generamos el principal
         $ret = 0;
         $ret = $this->db->insert('principals',array('uri'=>$principal_uri,'displayname'=>$username));
         if ($ret!=1) {
             die("NO se ha podido insertar el principal");
         }
+        */
         //Generamos el calendario default
         $ret = 0;
-        $ret = $this->db->insert('calendars',
-            array('principaluri'=>$principal_uri,'displayname'=>$this->default_calendar,
-            'synctoken'=>uniqid(),'uri'=>$this->default_calendar,'components'=>'VEVENT,VTODO'));        
+        $ret = $this->db->insert('oc_clndr_calendars',
+            array('userid'=>$username,'displayname'=>$this->default_calendar,
+                'uri'=>$this->default_calendar,'active'=>1,'ctag'=>1,'components'=>'VEVENT,VTODO'));        
         if ($ret!=1) {
             die("NO se ha podido insertar el calendar");
         }
@@ -70,17 +74,16 @@ class SabreMW
     public function delUser($username)
     {
         echo "Vamos a borrar el usuario $username";
-        $principal_uri = 'principals/'.$username;
+        
         //Obtenemos los calendarios
-        $calendars = $this->db->fetchAll('SELECT id FROM calendars WHERE principaluri = ?',array($principal_uri));
+        $calendars = $this->db->fetchAll('SELECT id FROM oc_clndr_calendars WHERE userid = ?',array($username));
         foreach($calendars as $calendar) {
             //Borramos los eventos
-            $this->db->delete('calendarobjects',array('calendarid'=>$calendar['id']));
+            $this->db->delete('oc_clndr_objects',array('calendarid'=>$calendar['id']));
             //Borramos el calendario
-            $this->db->delete('calendars',array('id'=>$calendar['id']));
+            $this->db->delete('oc_clndr_calendars',array('userid'=>$username));
         }
-        //Borramos de principals
-        $this->db->delete('principals',array('uri'=>$principal_uri));
+        $this->db->delete('oc_users',array('uid'=>$username));
     }
     
     public function getEvent($id) {
@@ -120,103 +123,64 @@ class SabreMW
         
 		//$vcalendar->add($event);
         $etag = md5($vcalendar->serialize());
-        $firstOccurence = $event->DTSTART->getDateTime()->getTimeStamp();
-        $lastOccurence = $firstOccurence;
+        $startdate = $event->DTSTART->getDateTime()->getTimeStamp();
+        $enddate = $startdate;
         $size = strlen($vcalendar->serialize());
         //echo "Tenemos el vclandera:<pre>";
         //echo $vcalendar->serialize();
         //echo "</pre>";
         
-        //echo "Vamos a introducir en BBDD el etag $etag el size $size y el tiempo $firstOccurence";
-        $ret= $this->db->insert('calendarobjects',array('calendardata'=>$vcalendar->serialize(),'uri'=>$uid.".ics",'calendarid'=>$calendar,'etag'=>$etag,
-            'size'=>$size,'componenttype'=>'VEVENT','firstoccurence'=>$firstOccurence,'lastoccurence'=>$lastOccurence,'lastmodified'=>$now->getTimestamp()));
+        //echo "Vamos a introducir en BBDD el etag $etag el size $size y el tiempo $startdate";
+        $ret= $this->db->insert('oc_clndr_objects',array('calendardata'=>$vcalendar->serialize(),
+            'uri'=>$uid.".ics",'calendarid'=>$calendar,
+            'startdate'=>$startdate,'enddate'=>$enddate,'lastmodified'=>$now->getTimestamp()));
         $id = $this->db->lastInsertId();
-       	//Actualizamos el synctoken del calendario
+       	//Actualizamos el ctag del calendario
         $this->updateSyncToken($calendar);
         
         //echo "Insercion hecha con ID: ".$id;
-        //echo "Vamos a introducir en BBDD el etag $etag el size $size y el tiempo $firstOccurence";
+        //echo "Vamos a introducir en BBDD el etag $etag el size $size y el tiempo $startdate";
         return $id;
-    }
-    
-    public function updateEvent($event_id,$calendar,$titulo,$descripcion,$fecha)
-    {
-        //FIXME esto debería ser una variable
-        $location="Uroges Cruces";
-        //echo "leemos el evento previo<br>";
-        //$evento = $this->db->fetchAssoc('SELECT * FROM calendarobjects WHERE id = ?', array($event_id));
-        //var_dump($evento);
-        //Borramos el evento previo
-        $ret = $this->db->delete('calendarobjects', array('id' => $event_id));
-        echo "Borrado evento previo con id ".$event_id." y resultado ".$ret;
-        //echo "Vamos a crear $calendar,$summary,$location,$fecha ";
-        $uid =  \Rhumsaa\Uuid\Uuid::uuid4();
-        $vcalendar = new \Sabre\VObject\Component\VCalendar();
-        $event = $vcalendar->add('VEVENT', [
-            'SUMMARY' => $titulo,
-            'DESCRIPTION' => $descripcion,
-            'DTSTART' => new \DateTime($fecha),
-            'LOCATION' => $location,
-            'UID' => $uid
-        ]);
-        
-		//$vcalendar->add($event);
-        $etag = md5($vcalendar->serialize());
-        $firstOccurence = $event->DTSTART->getDateTime()->getTimeStamp();
-        $lastOccurence = $firstOccurence;
-        $size = strlen($vcalendar->serialize());
-        
-        //echo "Tenemos el vclandera:<pre>";
-        //echo $vcalendar->serialize();
-        //echo "</pre>";
-        
-        //echo "Vamos a introducir en BBDD el etag $etag el size $size y el tiempo $firstOccurence";
-        $ret= $this->db->insert('calendarobjects',array('id'=>$event_id,'calendardata'=>$vcalendar->serialize(),'uri'=>$uid.".ics",'calendarid'=>$calendar,'etag'=>$etag,
-            'size'=>$size,'componenttype'=>'VEVENT','firstoccurence'=>$firstOccurence,'lastoccurence'=>$lastOccurence,'lastmodified'=>$now->format('c')));
-
-       	//Actualizamos el synctoken del calendario
-        $this->updateSyncToken($calendar);
-        //echo "Insercion hecha con ID: ".$event_id;
-        //echo "Vamos a introducir en BBDD el etag $etag el size $size y el tiempo $firstOccurence";
-        return $event_id;
     }
     
     public function delEvent($id)
     {
-        $event = $this->db->fetchAssoc('SELECT * FROM calendarobjects WHERE id = ?', array($id));
-        $ret = $this->db->delete('calendarobjects', array('id' => $id));
-        //Actualizamos el synctoken del calendario
+        $event = $this->db->fetchAssoc('SELECT * FROM oc_clndr_objects WHERE id = ?', array($id));
+        $ret = $this->db->delete('oc_clndr_objects', array('id' => $id));
+        //Actualizamos el ctag del calendario
         $this->updateSyncToken($event['calendarid']);
         if ($ret == 1) 
         {  
             //A borrado 1 columna 
+            echo "Borrado OK";
             return 1;
         }
         else
         {
+            echo "Borrado KO";
             return 0;
         }
         
     }
     
     public function updateSyncToken($calendar_id) {
-        $calendar = $this->db->fetchAssoc('SELECT * FROM calendars WHERE id = ?', array($calendar_id));
+        $calendar = $this->db->fetchAssoc('SELECT * FROM oc_clndr_calendars WHERE id = ?', array($calendar_id));
         //var_dump($calendar);
-        echo "Vamos a actualizar el synctoken de ".$calendar['principaluri']." qu ahora es". $calendar['synctoken']." --- ";
-        $new_synctoken = $calendar['synctoken']+1;
-        $ret = $this->db->update('calendars',array('synctoken'=>$new_synctoken),array('id'=>$calendar_id)); 
+        echo "Vamos a actualizar el ctag de ".$calendar['userid']." que ahora es". $calendar['ctag']." ---> ";
+        $new_ctag = $calendar['ctag']+1;
+        $ret = $this->db->update('oc_clndr_calendars',array('ctag'=>$new_ctag),array('id'=>$calendar_id)); 
         if ( $ret ==1 ) {
-            echo "Synctoken actualizado a ".$new_synctoken." con resultado".$ret."<br>";
+            echo "Ctag actualizado a ".$new_ctag." con resultado".$ret."<br>";
             return 1;
         } else {
-            echo "Fallo actualizando synctoken";
+            echo "Fallo actualizando ctag";
             return -1;
         }
         
     }
     
     public function getUserCalendar($username) {
-        $ret = $this->db->fetchAssoc('SELECT * FROM calendars WHERE principaluri = ?', array("principals/".$username));
+        $ret = $this->db->fetchAssoc('SELECT * FROM oc_clndr_calendars WHERE userid = ?', array($username));
         return $ret['id'];
     }
     
